7ed01c153466eaa729e19e8533f28ebc9ddaf5a2
sophiayau <124006972+sophiayau@users.noreply.github.com>
2025-01-05 22:00:40 -0500

    Started calendar. Still need to touch up styling, but I'm tired lol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CalendarContainer from './components/CalendarContainer';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <CalendarContainer></CalendarContainer>
       
     </div>
   );
---
+++ frontend/src/components/CalendarContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/CalendarContainer.css";
+import Calendar from "react-calendar";
+
+function CalendarContainer() {
+  return (
+    <div className="calendar-container">
+      <Calendar></Calendar>
+    </div>
+  );
+}
+
+export default CalendarContainer;
