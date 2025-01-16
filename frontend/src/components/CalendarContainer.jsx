7ccc44e0e2bd7f6046a671495334128a0f724e11
sophiayau <124006972+sophiayau@users.noreply.github.com>
2025-01-16 00:37:42 -0500

    Added handleDataChange func in CalendarContainer

--- frontend/src/components/CalendarContainer.jsx
+++ frontend/src/components/CalendarContainer.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/CalendarContainer.css";
 import Calendar from "react-calendar";
 import SidePanel from "./SidePanel";
 
 function CalendarContainer() {
+  // recognizes and logs which date is being selected by user
+  const handleDataChange = (date) => {
+    console.log("Selected Date:", date);
+  };
+
   return (
     <div className="calendar-container">
-      <Calendar></Calendar>
+      <Calendar onChange={handleDataChange} />
       <SidePanel></SidePanel>
     </div>
   );
